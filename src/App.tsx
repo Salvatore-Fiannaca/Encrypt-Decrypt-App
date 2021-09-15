cf0e38fe68cb747bc10e9077121ffd1feccfd497
Sasa <salv>
2021-09-15 17:42:32 +0200

    refactory

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,80 @@
 import { createAdapter, EncryptionAlgorithm } from "iocane";
+import React, { useState } from "react";
+import cn from 'classnames'
 
-export default function App() {
-  function encrypt(): void {
-    const msg: HTMLTextAreaElement = document.querySelector("#msg")!;
-    const key: HTMLTextAreaElement = document.querySelector("#key")!;
+export const App: React.FC = () => {
+  const [ message, setMessage] = useState<string>(""),
+  [ messageisValid, setValidMsg] = useState<boolean>(false),
+  [ key, setKey] = useState<string>(""),
+  [ keyisValid, setValidKey] = useState<boolean>(false),
+  [ btnCopyVisible, setBtnCopyVisible] = useState<boolean>(false)
 
-    if (!msg.value || !key.value) return;
-
-    createAdapter()
+  function encrypt() {
+    if (key && message) {
+      createAdapter()
       .setAlgorithm(EncryptionAlgorithm.CBC)
       .setDerivationRounds(14)
-      .encrypt(msg.value, key.value)
-      .then((res) => showResult(res.toString()));
+      .encrypt(message, key)
+      .then((res) => {
+        setMessage(res.toString())
+        setBtnCopyVisible(true)
+      })
+      .catch(() => {
+        setMessage("Something's gone wrong...")
+      })
+    }
   }
-  function decrypt(): void {
-    const msg: HTMLTextAreaElement = document.querySelector("#msg")!;
-    const key: HTMLTextAreaElement = document.querySelector("#key")!;
+  function decrypt() {
+    if (key && message){
+      createAdapter()
+      .decrypt(message, key)
+      .then((res) => {
+        setMessage(res.toString())
+        setBtnCopyVisible(true)
+      })
+      .catch(() => {
+        alert("Cannot decrypt this message...")
+      })
+    }
+  }
+  function changeHandler (e:any) {
+    if (e.target.type === 'password') {
+      setValidKey(e.target.value > 0)
+      setKey(e.target.value)
+    }
+    if (e.target.type === 'textarea') {
+      setValidMsg(e.target.value > 0)
+      setMessage(e.target.value)
+    }
 
-    if (!msg.value || !key.value) return;
+  }
+  function reset() {
+    setBtnCopyVisible(false)
+    setValidKey(false)
+    setValidMsg(false)
+    setMessage("");
+    setKey("");
+  }
+  function copyToClipboard() {
+    if (message) {
+      try {
+        if (window.isSecureContext) {
+          return navigator.clipboard.writeText(message)
+        }
 
-    createAdapter()
-      .decrypt(msg.value, key.value)
-      .then((res) => showResult(res.toString()));
-  }
-  function showResult(msg: string) {
-    const message: HTMLTextAreaElement = document.querySelector("#msg")!;
-    toggleBtn(true)
-    message.value = msg;
-  }
-  function toggleBtn(state: boolean):void {
-    const btn_copy: HTMLInputElement = document.querySelector("#btn_copy")!;
-    btn_copy.hidden = state ? false : true
-  }
-  function resetFields(): void {
-    const message: HTMLTextAreaElement = document.querySelector("#msg")!;
-    const key: HTMLInputElement = document.querySelector("#key")!;
-
-    toggleBtn(false)
-    message.value = "";
-    key.value = "";
-
-  }
-  function copyToClipboard(): void {
-    const message: HTMLTextAreaElement = document.querySelector("#msg")!;
-    navigator.clipboard.writeText(message.value)
+        var textArea = document.createElement("textarea");
+        textArea.value = message;
+        textArea.style.position = "fixed";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        document.execCommand('copy')
+        document.body.removeChild(textArea)
+        
+      } catch (error) {
+        alert("Cannot copy to clipboard")
+      } 
+    }
   }
 
   return (
@@ -52,22 +83,31 @@ export default function App() {
         <div className="form-group m-2">
           <label className="mb-2">Message:</label>
           <textarea
-            className="form-control"
-            id="msg"
+            className={cn(
+              "form-control",
+              {'is-valid': messageisValid},
+              {'is-invalid': !messageisValid}
+            )}
             placeholder="(text here)"
+            value={message}
+            onChange={changeHandler} 
           />
         </div>
         <div className="form-group m-2">
           <label className="mb-2">Key:</label>
           <input
             type="password"
-            className="form-control"
-            id="key"
+            className={cn(
+              "form-control",
+              {'is-valid': keyisValid},
+              {'is-invalid': !keyisValid}
+            )}
             placeholder="******"
+            value={key}
+            onChange={changeHandler} 
           />
         </div>
       </form>
-
       <div className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-auto py-3">
@@ -80,16 +120,24 @@ export default function App() {
               Decrypt
             </a>
           </div>
-          <div className="col-auto py-3">
-            <a className="btn btn-secondary" href="/#" onClick={resetFields}>
-              Reset
-            </a>
-          </div>
-          <div className="col-auto py-3">
-            <a className="btn btn-success" href="/#" id="btn_copy" hidden={true} onClick={copyToClipboard}>
-              Copy to Clipboard
-            </a>
-          </div>
+          
+          {
+            (key !== '' || message !== '') &&
+            <div className="col-auto py-3">
+              <a className="btn btn-secondary" href="/#" onClick={reset}>
+                Reset
+              </a>
+            </div>
+          }
+          {
+            ( message !== '' && btnCopyVisible ) && 
+            <div className="col-auto py-3">
+              <a className="btn btn-success" href="/#" onClick={copyToClipboard}>
+                Copy to Clipboard
+              </a>
+            </div>
+          }
+
         </div>
       </div>
     </div>
